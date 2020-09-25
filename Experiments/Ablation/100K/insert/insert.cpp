70382280ec3055a8967e19acba0da30cca342fca
Anonymous82338590 <anonymous82338590@gmail.com>
2020-09-25 16:03:57 +0800

    ablation 100K

--- Experiments/Ablation/100K/insert/insert.cpp
+++ Experiments/Ablation/100K/insert/insert.cpp
@@ -8,9 +8,9 @@
 #include <chrono>
 #include <random>
 #include <algorithm>
-#include "../../../HeaderFiles/DAnolimit.h"
-#include "../../../HeaderFiles/DAstatic.h"
-#include "../../../HeaderFiles/DynamicArray.h"
+#include "../../../../HeaderFiles/DAnolimit.h"
+#include "../../../../HeaderFiles/DAstatic.h"
+#include "../../../../HeaderFiles/DynamicArray.h"
 using namespace std;
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
 #define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
@@ -18,7 +18,7 @@ typedef std::chrono::high_resolution_clock::time_point TimeVar;
 bool CompareArray(const int *a, const int *b, int len) {
     if (a == nullptr || b == nullptr) {
         cout<<"null pointer error!" << endl;
-        return 0 ;
+        return false ;
     }
     for (int i = 0; i < len; ++i) {
         if (a[i] != b[i]) {
@@ -38,7 +38,9 @@ inline std::mt19937& generator() {
 int RandomInt(int min, int max) {
     std::uniform_int_distribution<int> dist(min, max);
     return dist(generator());
+   // return (rand() % (max-min+1))+ min;
 }
+
 
 int* RangeDistributionRandom( int num, int min, int max) {
     int * n = new int[num];
@@ -49,29 +51,26 @@ int* RangeDistributionRandom( int num, int min, int max) {
     return n;
 }
 
-int main() {
+int main(int argc, char** argv) {
     //var time1, time2 time.Time
-    string filepath[3] = {"move.csv", "moveLog.txt"};
+    string filepath[3];// = {"insert.csv", "insertLog.txt"};
+    filepath[0] = argv[1];
+    filepath[1] = argv[2];
     ofstream finstant, flog, ffinal;
     finstant.open(filepath[0], ios::out | ios::in | ios::trunc);
     flog.open(filepath[1], ios::out | ios::in | ios::trunc);
     //ffinal.open(filepath[2], ios::out | ios::in | ios::trunc);
     finstant<<" ,DA,DANL,ST"<<endl;
 
-    int iniNum = 100000; //0.03 = 30000
+    int iniNum = 100000;
     int danodesize = 50;
-    int m = 500;  //for linked list
-    int NumOfOutput = 10;
     int operations = 100000;
-    int InsertActions = operations * 2 / 10;
-    int DeleteActions = operations * 2 / 10;
-    int ReorderActions = operations * 2 / 10;
-    int SwapActions = operations * 2 / 10;
-    int MoveActions = operations * 2 / 10;
-    int TotalActions = DeleteActions + InsertActions + ReorderActions+SwapActions + MoveActions; // 550 000
-    int ComputeInterval = TotalActions / NumOfOutput; // 10%
-    int Interval100 = TotalActions / 100;
-    printf("%d, %d, %d, %d, %d\n", InsertActions, DeleteActions, ReorderActions, SwapActions, MoveActions);
+    int InsertActions = operations * 25 / 100;
+    int DeleteActions = operations * 25 / 100;
+    int ReorderActions = operations * 25 / 100;
+    int SwapActions = operations * 25 / 100;
+    int TotalActions = DeleteActions + InsertActions + ReorderActions+SwapActions ;
+    printf("%d, %d, %d, %d\n", InsertActions, DeleteActions, ReorderActions, SwapActions);
     printf("# of operations = %d\n", operations);
     int CurOutputNum = 0;
 
@@ -85,10 +84,6 @@ int main() {
         a[ua] = 5;
         ua++;
     }
-    for (int y = 0; y < MoveActions; y++) {
-        a[ua] = 6;
-        ua++;
-    }
     for (int y = 0; y < InsertActions; y++) {
         a[ua] = 2;
         ua++;
@@ -97,6 +92,7 @@ int main() {
         a[ua] = 3;
         ua++;
     }
+
 
     shuffle(a, a + TotalActions, generator());
     //random_shuffle(&a[DeleteActions+InsertActions], &a[TotalActions]);
@@ -117,14 +113,13 @@ int main() {
     DAnolimit * danolimit = NewDAnolimit(array, iniNum, danodesize);
     DAstatic * stree = NewStree(array, iniNum, danodesize);
     DynamicArray * da = NewDynamicArray(array, iniNum, danodesize);
-    //ll->PrintStructure();
     delete []array;
     int NumDA, NumDANL, NumST;
-    //da->PrintTree();
     cout<<"da depth = "<<da->Depth() << endl;
-    int DAdepth = 1, DANLdepth = 1, DAODPdepth = 1, STdepth = stree->Depth();
+    int DAdepth = 1, DANLdepth = 1, DAodpdepth = 1, STdepth = stree->Depth();
     int numUpdate = 0;
     for (int lt = 0; lt < TotalActions; lt++) {
+        //printf("lt = %d\n", lt);
         if (lt % 5000 == 0) {
             cout<<"lt = "<<lt;
             cout<<"da depth = "<<da->Depth() << endl;
@@ -179,15 +174,11 @@ int main() {
                 if (end >= NowTotalNum) {
                     end = NowTotalNum - 1;
                 }
-                //cout<<"reorder "<<start<<"  "<<end<<endl;
-                //cout<<"new order: ";
                 int * oldArray = da->RangeQuery(start, end, &NumDA);
                 int * newArray = new int[len];
                 for (int j = 0; j < len; ++j) {
                     newArray[j] = oldArray[len-j-1];
-                    //cout<<newArray[j]<<" ";
                 }
-                // cout<<endl;
 
                 danolimit->Reorder(start, end, newArray);
                 da->Reorder(start, end, newArray);
@@ -199,133 +190,68 @@ int main() {
             }
             case 5: //swap
             {
-                int a[4] = {};
-                for (int & j : a) {
+                int b[4] = {};
+                for (int & j : b) {
                     j = RandomInt(1, NowTotalNum);
                 }
-                sort(a, a+4);
-                if (a[1] == a[2]) {
+                sort(b, b + 4);
+                if (b[1] == b[2]) {
                     continue;
                 }
-                int start1 = a[0];
-                int end1 = a[1];
-                int start2 = a[2];
-                int end2 = a[3];
+                int start1 = b[0];
+                int end1 = b[1];
+                int start2 = b[2];
+                int end2 = b[3];
                 danolimit->Swap(start1, end1, start2, end2);
                 da->Swap(start1, end1, start2, end2);
                 stree->Swap(start1, end1, start2, end2);
 
                 break;
             }
-            case 6: //move
-            {
-                int a[3] = {};
-                for (int & j : a) {
-                    j = RandomInt(1, NowTotalNum);
-                }
-                sort(a, a+3);
-                if (a[1] == a[2]) {
-                    continue;
-                }
-                int start1, end1, start2;
 
-                if (RandomInt(1, 10) % 2 == 1) {
-                    start1 = a[0];
-                    end1 = a[1];
-                    start2 = a[2];
-                } else {
-                    start1 = a[1];
-                    end1 = a[2];
-                    start2 = a[0];
-                }
-                danolimit->Move(start1, end1, start2);
-                da->Move(start1, end1, start2);
-                stree->Move(start1, end1, start2);
-
-                break;
-            }
         }
 
         if ( (lt+1 <= 10) || ((lt+1<=100) && ((lt+1)%10 == 0)) || ((lt+1<=1000) && ((lt+1)%100 == 0))
              || ((lt+1<=10000) && ((lt+1)%1000 == 0)) || ((lt+1<=100000) && ((lt+1)%10000 == 0))
              || ((lt+1<=1000000) && ((lt+1)%100000 == 0))) {
-
-            int num;
-            /*
-            int * AnsDA = da->RangeQuery(1, NowTotalNum, &num);
-            int * Ansdanl = danolimit->RangeQuery(1, NowTotalNum, &num);
-            int * Ansst = stree->RangeQuery(1, NowTotalNum, &num);
-            int * Ansdaodp = daonedp->RangeQuery(1, NowTotalNum, &num);
-            bool flag1 = false, flag2 = false, flag3 = false, flag4 = false;
-            if (!CompareArray(AnsDA, Ansdanl, num)) {
-                flag1 =true;
-            }
-            if (!CompareArray(AnsDA, Ansst, num)) {
-                flag2 =true;
-            }
-            if (!CompareArray(Ansst, Ansdanl, num)) {
-                flag3 =true;
-            }
-            if (!CompareArray(Ansst, Ansdaodp, num)) {
-                flag4 =true;
-            }
-            if (flag4||flag3||flag2||flag1) {
-                printf(" ---------------------lt = %d, move not equal: %d %d %d %d\n", lt, flag1, flag2, flag3, flag4);
-                //stree->PrintTree();
-                break;
-            }
-
-            delete []Ansst, delete []Ansdanl, delete []AnsDA, delete []Ansdaodp;
-*/
             numUpdate ++;
 
             long long Tda = 0, Tdanl = 0, Tst = 0, Tdaodp = 0;
             CurOutputNum ++;
             double fl = (lt+1)*1.0/operations ;
             finstant << fl << ",";
-            int start1 = 7000, end1 = 56000, start2 = 91000;
-            /*
-               int a[4] = {};
-               for (int j = 0; j < 4; ++j) {
-                   a[j] = RandomInt(1, NowTotalNum);
-               }
-               sort(a, a+4);
-               if (a[1] == a[2]) {
-                   continue;
-               }
-               int start1 = a[0];
-               int end1 = a[1];
-               int start2 = a[2];
-               int end2 = a[3];
-            */
+            int pos = RandomInt(1, NowTotalNum);
 
             time1 = timeNow();
-            danolimit->Move(start1, end1, start2);
+            danolimit->Insert(ToInsert, pos);
             time2 = timeNow();
             Tdanl = duration(time2-time1);
 
             time1 = timeNow();
-            da->Move(start1, end1, start2);
+            da->Insert(ToInsert, pos);
             time2 = timeNow();
             Tda = duration(time2-time1);
 
             time1 = timeNow();
-            stree->Move(start1, end1, start2);
+            stree->Insert(ToInsert, pos);
             time2 = timeNow();
             Tst = duration(time2 - time1);
 
-            finstant <<Tda<< ","<< Tdanl<<","<<Tst<<endl;
+            ToInsert++;
+            NowTotalNum++;
+            finstant <<Tda << ","<< Tdanl<<","<<Tst<<endl;
             cout<<"lt = "<< lt <<" da depth = "<<da->Depth()<<endl;
         }
         numUpdate++;
     } //for lt <= loopTime
-    //cout<<"da depth = "<<da->Depth() << endl;
     flog<<"da depth = "<<da->Depth() << endl;
     delete []reorderrange;
     delete []a;
+    /*
     delete da;
     delete danolimit;
     delete stree;
+     */
     flog.close();
     finstant.close();
     return 0;
--- Experiments/Ablation/100K/reorder/reorder.cpp
+++ Experiments/Ablation/100K/reorder/reorder.cpp
@@ -8,9 +8,9 @@
 #include <chrono>
 #include <random>
 #include <algorithm>
-#include "../../../HeaderFiles/DAnolimit.h"
-#include "../../../HeaderFiles/DAstatic.h"
-#include "../../../HeaderFiles/DynamicArray.h"
+#include "../../../../HeaderFiles/DAnolimit.h"
+#include "../../../../HeaderFiles/DAstatic.h"
+#include "../../../../HeaderFiles/DynamicArray.h"
 using namespace std;
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
 #define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
@@ -18,7 +18,7 @@ typedef std::chrono::high_resolution_clock::time_point TimeVar;
 bool CompareArray(const int *a, const int *b, int len) {
     if (a == nullptr || b == nullptr) {
         cout<<"null pointer error!" << endl;
-        return 0 ;
+        return false ;
     }
     for (int i = 0; i < len; ++i) {
         if (a[i] != b[i]) {
@@ -51,28 +51,24 @@ int* RangeDistributionRandom( int num, int min, int max) {
 
 int main() {
     //var time1, time2 time.Time
-    string filepath[3] = {"pointquery.csv", "pointqueryLog.txt"};
+    string filepath[3] = {"reorder.csv", "reorderLog.txt"};
     ofstream finstant, flog, ffinal;
     finstant.open(filepath[0], ios::out | ios::in | ios::trunc);
     flog.open(filepath[1], ios::out | ios::in | ios::trunc);
     //ffinal.open(filepath[2], ios::out | ios::in | ios::trunc);
     finstant<<" ,DA,DANL,ST"<<endl;
 
-    int iniNum = 100000; //0.03 = 30000
+    int reorderLength = 100;
+
+    int iniNum = 100000;
     int danodesize = 50;
-    int CbtOrder = 50;
-    int m = 500;  //for linked list
-    int NumOfOutput = 10;
     int operations = 100000;
-    int InsertActions = operations * 2 / 10;
-    int DeleteActions = operations * 2 / 10;
-    int ReorderActions = operations * 2 / 10;
-    int SwapActions = operations * 2 / 10;
-    int MoveActions = operations * 2 / 10;
-    int TotalActions = DeleteActions + InsertActions + ReorderActions+SwapActions + MoveActions; // 550 000
-    int ComputeInterval = TotalActions / NumOfOutput; // 10%
-    int Interval100 = TotalActions / 100;
-    printf("%d, %d, %d, %d, %d\n", InsertActions, DeleteActions, ReorderActions, SwapActions, MoveActions);
+    int InsertActions = operations * 25 / 100;
+    int DeleteActions = operations * 25 / 100;
+    int ReorderActions = operations * 25 / 100;
+    int SwapActions = operations * 25 / 100;
+    int TotalActions = DeleteActions + InsertActions + ReorderActions+SwapActions ;
+    printf("%d, %d, %d, %d\n", InsertActions, DeleteActions, ReorderActions, SwapActions);
     printf("# of operations = %d\n", operations);
     int CurOutputNum = 0;
 
@@ -84,10 +80,6 @@ int main() {
     }
     for (int y = 0; y < SwapActions; y++) {
         a[ua] = 5;
-        ua++;
-    }
-    for (int y = 0; y < MoveActions; y++) {
-        a[ua] = 6;
         ua++;
     }
     for (int y = 0; y < InsertActions; y++) {
@@ -104,11 +96,10 @@ int main() {
     //random_shuffle(&a[0], &a[MoveActions+SwapActions+ReorderActions]);
     //random_shuffle(&a[0], &a[operations]);
     //int * shortqueryrange = RangeDistributionRandom(, 1, 100);
-    int * reorderrange = RangeDistributionRandom(ReorderActions, 1, 1000);
+    int * reorderrange = RangeDistributionRandom(ReorderActions, 1, 100);
     int ir = 0;
 
     TimeVar time1, time2;
-
 
     int NowTotalNum = iniNum;
     int ToInsert = iniNum + 1;
@@ -116,18 +107,16 @@ int main() {
     for (int i = 0; i < iniNum; ++i) {
         array[i] = i+1;
     }
+
     DAnolimit * danolimit = NewDAnolimit(array, iniNum, danodesize);
     DAstatic * stree = NewStree(array, iniNum, danodesize);
     DynamicArray * da = NewDynamicArray(array, iniNum, danodesize);
-
     delete []array;
-    int NumSA, NumDA, Numll, NumCBT;
-    //da->PrintTree();
+    int NumDA;
     cout<<"da depth = "<<da->Depth() << endl;
-    int depth = 1;
-    int DAdepth = 1, DANLdepth = 1, DAODPdepth = 1, STdepth = stree->Depth();
+    int DAdepth = 1, DANLdepth = 1, STdepth = stree->Depth();
     int numUpdate = 0;
-    for (int lt = 0; lt < operations; lt++) {
+    for (int lt = 0; lt < TotalActions; lt++) {
         if (lt % 5000 == 0) {
             cout<<"lt = "<<lt;
             cout<<"da depth = "<<da->Depth() << endl;
@@ -182,13 +171,11 @@ int main() {
                 if (end >= NowTotalNum) {
                     end = NowTotalNum - 1;
                 }
-                int * oldArray = da->RangeQuery(start, end, &NumSA);
+                int * oldArray = da->RangeQuery(start, end, &NumDA);
                 int * newArray = new int[len];
                 for (int j = 0; j < len; ++j) {
                     newArray[j] = oldArray[len-j-1];
-                    //cout<<newArray[j]<<" ";
                 }
-                // cout<<endl;
 
                 danolimit->Reorder(start, end, newArray);
                 da->Reorder(start, end, newArray);
@@ -200,91 +187,75 @@ int main() {
             }
             case 5: //swap
             {
-                int a[4] = {};
-                for (int & j : a) {
+                int b[4] = {};
+                for (int & j : b) {
                     j = RandomInt(1, NowTotalNum);
                 }
-                sort(a, a+4);
-                if (a[1] == a[2]) {
+                sort(b, b + 4);
+                if (b[1] == b[2]) {
                     continue;
                 }
-                int start1 = a[0];
-                int end1 = a[1];
-                int start2 = a[2];
-                int end2 = a[3];
+                int start1 = b[0];
+                int end1 = b[1];
+                int start2 = b[2];
+                int end2 = b[3];
                 danolimit->Swap(start1, end1, start2, end2);
                 da->Swap(start1, end1, start2, end2);
                 stree->Swap(start1, end1, start2, end2);
 
                 break;
             }
-            case 6: //move
-            {
-                int a[3] = {};
-                for (int & j : a) {
-                    j = RandomInt(1, NowTotalNum);
-                }
-                sort(a, a+3);
-                if (a[1] == a[2]) {
-                    continue;
-                }
-                int start1, end1, start2;
-
-                if (RandomInt(1, 10) % 2 == 1) {
-                    start1 = a[0];
-                    end1 = a[1];
-                    start2 = a[2];
-                } else {
-                    start1 = a[1];
-                    end1 = a[2];
-                    start2 = a[0];
-                }
-
-                danolimit->Move(start1, end1, start2);
-                da->Move(start1, end1, start2);
-                stree->Move(start1, end1, start2);
-
-                break;
-            }
         }
+        numUpdate++;
 
         if ( (lt+1 <= 10) || ((lt+1<=100) && ((lt+1)%10 == 0)) || ((lt+1<=1000) && ((lt+1)%100 == 0))
              || ((lt+1<=10000) && ((lt+1)%1000 == 0)) || ((lt+1<=100000) && ((lt+1)%10000 == 0))
              || ((lt+1<=1000000) && ((lt+1)%100000 == 0))) {
-
-            long long Tda = 0, Tdanl = 0, Tst = 0, Todp = 0;
+            int num;
+            long long Tda = 0, Tdanl = 0, Tst = 0;
             CurOutputNum ++;
             double fl = (lt+1)*1.0/operations ;
             finstant << fl << ",";
-            int pos = 30000, ans;
-            //int pos = RandomInt(1, NowTotalNum);
+            int start1, end1 = NowTotalNum + 2;
+            int repeatNum = 10;
+            for (int k = 0; k < repeatNum; ++k) {
+                while (end1 >= NowTotalNum) {
+                    start1 = RandomInt(1, NowTotalNum);
+                    end1 = start1 + reorderLength - 1;
+                }
+                int numOldArray;
+                int * oldArray = da->RangeQuery(start1, end1, &numOldArray);
+                int * newArray = new int[numOldArray];
+                for (int j = 0; j < numOldArray; ++j) {
+                    newArray[j] = oldArray[numOldArray-1-j];
+                }
 
-            time1 = timeNow();
-            ans = danolimit->Query(pos);
-            time2 = timeNow();
-            Tdanl = duration(time2-time1);
+                time1 = timeNow();
+                danolimit->Reorder(start1, end1, newArray);
+                time2 = timeNow();
+                Tdanl += duration(time2-time1);
 
-            time1 = timeNow();
-            ans = da->Query(pos);
-            time2 = timeNow();
-            Tda = duration(time2-time1);
+                time1 = timeNow();
+                da->Reorder(start1, end1, newArray);
+                time2 = timeNow();
+                Tda += duration(time2-time1);
 
-            time1 = timeNow();
-            ans = stree->Query(pos);
-            time2 = timeNow();
-            Tst = duration(time2 - time1);
+                time1 = timeNow();
+                stree->Reorder(start1, end1, newArray);
+                time2 = timeNow();
+                Tst += duration(time2 - time1);
 
-            finstant <<Tda << ","<< Tdanl<<","<<Tst<<endl;
+                delete []newArray;
+                delete []oldArray;
+            }
+
+            finstant <<Tda/repeatNum << ","<< Tdanl/repeatNum <<","<< Tst/repeatNum <<endl;
             cout<<"lt = "<< lt <<" da depth = "<<da->Depth()<<endl;
         }
     } //for lt <= loopTime
-    //cout<<"da depth = "<<da->Depth() << endl;
     flog<<"da depth = "<<da->Depth() << endl;
     delete []reorderrange;
     delete []a;
-    delete da;
-    delete stree;
-    delete danolimit;
     flog.close();
     finstant.close();
     return 0;
